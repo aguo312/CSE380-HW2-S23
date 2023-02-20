294ed056a4ed5e99e5fe1137cb4c30a4d0b71d04
aguo312 <andrew.guo@stonybrook.edu>
2023-02-20 13:54:48 -0500

    Part 7.1

--- src/hw2/shaders/LaserShaderType.ts
+++ src/hw2/shaders/LaserShaderType.ts
@@ -63,6 +63,10 @@ export default class LaserShaderType extends RectShaderType {
 		const u_Transform = gl.getUniformLocation(program, "u_Transform");
 		gl.uniformMatrix4fv(u_Transform, false, transformation.toArray());
 
+		let webGL_color = options.color.toWebGL();
+		const laser_Color = gl.getUniformLocation(program, "laser_Color");
+		gl.uniform4f(laser_Color, webGL_color[0], webGL_color[1], webGL_color[2], webGL_color[3]);
+
 		// Draw the quad
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 	}
@@ -72,6 +76,7 @@ export default class LaserShaderType extends RectShaderType {
 			position: gc.position,
 			size: gc.size,
 			rotation: gc.rotation,
+			color: gc.color
 		}
 		return options;
 	}
